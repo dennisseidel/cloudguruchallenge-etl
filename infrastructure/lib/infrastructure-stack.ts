d01a0bcb24405caca2326de7cd4e8d5681538d09
Dennis Seidel <den.seidel@gmail.com>
2020-10-11 19:44:58 +0200

    feat: sns topic & email subscription

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -1,9 +1,12 @@
 import * as cdk from '@aws-cdk/core';
+import * as sns from '@aws-cdk/aws-sns';
+import * as subscriptions from '@aws-cdk/aws-sns-subscriptions';
 
 export class InfrastructureStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const etlNotificationTopic = new sns.Topic(this, 'etlNotificationTopic');
+    etlNotificationTopic.addSubscription(new subscriptions.EmailSubscription('den.seidel@gmail.com'));
   }
 }
